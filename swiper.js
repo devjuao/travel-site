23ddb6606b515414248d64d6ddc7d0d20a8845a5
João Helder <joaohelderdev@gmail.com>
2026-05-02 12:20:51 -0300

    first commit

---
+++ swiper.js
@@ -0,0 +1,38 @@
+const swiper = new Swiper('.box', {
+  // Optional parameters
+  slidesPerView: 5, 
+  spaceBetween: 30,
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true,
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: '.swiper-scrollbar',
+  },
+
+  breakpoints: {
+        0: {
+            slidesPerView: 1,
+        },
+        768: {
+            slidesPerView: 2,
+        },
+        1024: {
+            slidesPerView: 3,
+        }
+    }
+
+});
+
